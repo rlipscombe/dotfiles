40ce90178b9f61a2689c4b1581b829854966bc63
Roger Lipscombe <roger@differentpla.net>
2022-06-06 16:08:33 +0100

    Symlink git and tmux configs

--- install.zsh
+++ install.zsh
@@ -8,3 +8,6 @@ ln -sf $dir/zshrc $HOME/.zshrc
 
 ln -sf $dir/oh-my-zsh/plugins/rlipscombe-git $HOME/.oh-my-zsh/plugins
 ln -sf $dir/oh-my-zsh/themes/rlipscombe.zsh-theme $HOME/.oh-my-zsh/themes
+
+ln -sf $dir/tmux.conf $HOME/.tmux.conf
+ln -sf $dir/gitconfig $HOME/.gitconfig
