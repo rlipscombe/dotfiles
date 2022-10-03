90068e5c5292937829c796e360aabba18c76fc73
Roger Lipscombe <roger@differentpla.net>
2022-10-03 15:52:03 +0100

    Add Homebrew
    It'll probably need skipping if not installed, but this'll do for now.

--- install.zsh
+++ install.zsh
@@ -5,6 +5,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 dir=${0:a:h}
 
 ln -sf $dir/zshrc $HOME/.zshrc
+ln -sf $dir/zprofile $HOME/.zprofile
 
 ln -sf $dir/oh-my-zsh/plugins/rlipscombe-git $HOME/.oh-my-zsh/plugins
 ln -sf $dir/oh-my-zsh/themes/rlipscombe.zsh-theme $HOME/.oh-my-zsh/themes
